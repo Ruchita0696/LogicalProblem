2c09dab2d84fd3cf3f4bd5c8882fa936c1d5c37c
ruchi06 <ruchi06@gmail.com>
2023-01-06 17:44:02 +0530

    Fibonacci Series

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of Elements : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            FibonacciSeries series = new FibonacciSeries();
+            series.CalculateFibonacciSeries();
+        }
+    }
+}
