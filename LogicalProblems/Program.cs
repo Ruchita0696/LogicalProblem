c88a29ca4c62be7494f6418565ed57d9a03a6e1b
ruchi06 <ruchi06@gmail.com>
2023-01-06 19:08:44 +0530

    Prime Number

--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number to Check Number is Perfect or Not : ");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number to Check Number is Prime or Not : ");
+            int pn = Convert.ToInt32(Console.ReadLine());
 
-            PerfectNumber perfect = new PerfectNumber();
-            perfect.CheckPerfectNumber(n);
+            PrimeNumber prime = new PrimeNumber();
+            prime.CheckPrimeNumber(pn);
         }
     }
 }
